d655e0796278f74dabce05b3ac0074fc82598a72
Braden Hechmer <78294576+ebhechmer@users.noreply.github.com>
2022-10-02 09:19:49 -0400

    no longer needed

--- uhoh/bar.py
+++
@@ -1,9 +0,0 @@
-class Bar(object):
-    def __init__(self, key_signature="c"):
-        self.primitives = []
-
-    def addPrimitive(self, primitive):
-        self.primitives.append(primitive)
-
-    def getPrimitives(self):
-        return self.primitives
